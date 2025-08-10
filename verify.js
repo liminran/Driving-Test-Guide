9c8c5b2b3c74db2c2905e6a9803dd3433dcfd111
Min <2629744779@qq.com>
2025-08-11 02:08:56 +0800

    Delete verify.js

--- verify.js
+++
@@ -1,5 +0,0 @@
-  const fs=require('fs');const crypto=require('crypto');
-  const jws=process.argv[2];const [h,p,s]=jws.split('.');
-  const verify=crypto.createVerify('RSA-SHA256');verify.update(`${h}.${p}`);verify.end();
-  const ok=verify.verify(fs.readFileSync('public.pem'), Buffer.from(s.replace(/-/g,'+').replace(/_/g,'/'),'base64'));
-  console.log('verify=',ok);
